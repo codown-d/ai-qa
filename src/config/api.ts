b665f121307320ca4415a1d2b7a0f2fcbb6393f9
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-07-10 23:34:22 +0800

    处理bug

--- src/config/api.ts
+++ src/config/api.ts
@@ -34,12 +34,23 @@ export const rename = (name?: any, data?: any) =>
     },
   });
 
-const MESSAGEAPI = "message";
 export const getHistoricMessage = (data?: any) =>
-  http.get(`${MESSAGEAPI}/kb?kb_name=${data}`);
-export const setOpinion = (data?: any) =>
-  http.put(`${MESSAGEAPI}/${data.id}?feedback=${data.feedback}`);
-export const getMessageRecord = () => http.get(`${MESSAGEAPI}/kb/month`);
+  http.get(`/message/kb?kb_name=${data}`);
+  
+export const setOpinion = (data: { id: any; feedback: any }) => {
+  let { id, feedback } = data;
+  return http.put(`/message/${id}?feedback=${feedback}`);
+};
+export const getMessageRecord = () => http.get(`/message/kb/month`);
+export const getAIResponse = (data?: any) => http.post(`/bot/chat`, data);
 
-const BOTAPI = "bot";
-export const getAIResponse = (data?: any) => http.post(`${BOTAPI}/chat`, data);
+//更新内容配置
+//http://47.115.148.227:8000/api/dict/gznr
+export const getDictGznr = (data?: any) => http.get(`/api/dict/gznr`, data);
+
+//常见问题解答配置
+//http://47.115.148.227:8000/api/dict/wtjd
+export const getDictWtjd = (data?: any) => http.get(`/api/dict/wtjd`, data);
+//隐私政策，使用条款配置
+//http://47.115.148.227:8000/api/dict/ystk
+export const getDictYstk = (data?: any) => http.get(`/api/dict/ystk`, data);
